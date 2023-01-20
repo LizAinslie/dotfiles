d89145b123a9ce7235ba6ce6afcf66cc2866bfbb
Liz Ainslie <lizainslie16@gmail.com>
2023-01-20 04:56:03 -0700

    chore(config): fish gpg tty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x GPG_TTY (tty)
+
 function fish_prompt
 	printf '[%s%s%s@%s %s%s%s]%s%s%s %% ' (set_color yellow) $USER (set_color green) $hostname (set_color blue) (prompt_pwd) (set_color normal) (set_color brblue) (fish_git_prompt) (set_color normal)
 end
