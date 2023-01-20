0b6d0793c195dabed8e6f9186a15c70af67261d0
Liz Ainslie <lizainslie16@gmail.com>
2023-01-20 00:35:47 -0700

    feat(config): update configs & scripts

--- backup.sh
+++ backup.sh
@@ -10,3 +10,15 @@ sed -i '/signingkey/d' ./git/.config/git/config
 
 # backup fish configs
 cp ~/.config/fish/config.fish ./fish/.config/fish/config.fish
+
+# backup neofetch configs
+cp -r ~/.config/neofetch/* ./neofetch/.config/neofetch
+
+# backup picom config
+cp ~/.config/picom.conf ./picom/.config/picom.conf
+
+# backup i3 config
+cp ~/.config/i3/config ./i3/.config/i3/config
+
+# backup eww configs
+cp -r ~/.config/eww/* ./eww/.config/eww
--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-# Fetch repo indexes and install base packages
-pacman -Sy spectacle git fish neovim w3m neofetch
-
-# Restore wallpaper
+# restore wallpaper
 cp ./extra/wallpaper.png /wallpaper.png
 
-# Restore git configs
+# restore git configs
 cp -r ./git/.config/git/* ~/.config/git/
 
-# Restore fish configs
+# restore fish configs
 cp ./fish/.config/fish/config.fish ~/.config/fish/config.fish
+
+# restore neofetch configs
+cp -r ./neofetch/.config/neofetch/* ~/.config/neofetch/
+
+# restore picom config
+cp ./picom/.config/picom.conf ~/.config/picom.conf
+
+# restore i3 config
+cp ./i3/.config/i3/config ~/.config/i3/config
+
+# restore eww configs
+cp -r ./eww/.config/eww/* ~/.config/eww/
