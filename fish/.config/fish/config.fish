8c9ee00be4ab6d780cabe205e7bfdba769a8fdea
Liz Ainslie <lizainslie16@gmail.com>
2023-02-15 06:30:41 -0700

    feat(nvim): add discord rpc

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,8 @@ set -x GPG_TTY (tty)
 function fish_prompt
 	printf '[%s%s%s@%s %s%s%s]%s%s%s %% ' (set_color yellow) $USER (set_color green) $hostname (set_color blue) (prompt_pwd) (set_color normal) (set_color brblue) (fish_git_prompt) (set_color normal)
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/lizzy/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
